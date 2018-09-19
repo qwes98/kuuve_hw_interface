46e436059353c0d827ef44d50f9f7ea1e5f3819e
Jiwon Park <qwes8873@gmail.com>
2018-09-19 21:23:34 +0900

    Fix some runtime error

--- src/kuuvef1_hw_interface_node.cpp
+++ src/kuuvef1_hw_interface_node.cpp
@@ -7,12 +7,14 @@ int main(int argc, char** argv)
 
   // NOTE: We run the ROS loop in a separate thread as external calls such
   // as service callbacks to load controllers can block the (main) control loop
-  ros::AsyncSpinner spinner(1);
+  ros::AsyncSpinner spinner(2);
   spinner.start();
 
   kuuvef1_hw_interface::Kuuvef1HardwareInterface kuuvef1(nh);
 
-  ros::spin();
+  ros::waitForShutdown();
+
+  //ros::spin();
 
   return 0;
 }
